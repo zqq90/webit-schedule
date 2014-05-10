cfa93137a79d3b6f37ef41d217b99b36bccc1a75
zqq90 <zqq_90@163.com>
2014-05-10 16:18:11 +0800

    Update CronParser

--- src/main/java/webit/schedule/core/CronParser.java
+++ src/main/java/webit/schedule/core/CronParser.java
@@ -26,6 +26,7 @@ import webit.schedule.core.matcher.YearMatcher;
 import webit.schedule.util.IntArrayList;
 
 /**
+ * Cron Parser
  *
  * @author zqq90
  */
@@ -283,21 +284,10 @@ public class CronParser {
         char c;
         while (offset < to) {
             c = buffer[offset++];
-            switch (c) {
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    value = value * 10 + ((int) c - (int) '0');
-                    break;
-                default:
-                    throw createInvalidCronException("Invalid numberic char '" + c + '\'', offset);
+            if (c >= '0' && c <= '9') {
+                value = value * 10 + ((int) c - (int) '0');
+            } else {
+                throw createInvalidCronException("Invalid numberic char '" + c + '\'', offset);
             }
         }
         return value;
